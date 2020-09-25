b9f72afd2c56e342d3d3332421defc89f8fb3757
vkuzel <vkuzel@gmail.com>
2020-09-25 14:11:02 +0200

    * Run upgrade in the Java application in a loop, waiting for a new version to occur. * Redirect upgrade process I/O to application so it can be logged.

--- src/DebianPackageDemo.java
+++ src/DebianPackageDemo.java
@@ -4,25 +4,28 @@ import com.sun.security.auth.module.UnixSystem;
 public class DebianPackageDemo {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		System.out.println("Application started.");
+		// Run application in a loop and schedule upgrade every half a minute.
+		// Scheduling upgrade does not mean that application will upgrade. If
+		// there is not new version and APT sources list has not been updated
+		// then there will be nothing to upgrade.
+		while (true) {
+			Thread.sleep(30_000);
+			upgrade();
+		}
+	}
 
-		// Sleep for a while and then upgrade application.
-		Thread.sleep(30_000);
-
-		System.out.println("Schedule upgrade to new version.");
-		String[] command = new String[] {"/bin/sh", "-c", "systemd-run --user --on-active=1 /usr/lib/debian-package-demo/debian-package-demo-upgrade.sh"};
+	private static void upgrade() throws IOException {
+		System.out.println("Schedule upgrade...");
+		String scheduleUpgradeCommand = "systemd-run --user --on-active=1 /usr/lib/debian-package-demo/debian-package-demo-upgrade.sh";
+		ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", scheduleUpgradeCommand);
 		// Environment variable XDG_RUNTIME_DIR has to be set for
 		// systemd-run to run correctly. Otherwise "Failed to create
 		// bus connection" error will occur.
-		String xdgRuntimeDir = "XDG_RUNTIME_DIR=/run/user/" + new UnixSystem().getUid();
-		String[] environment = new String[] {};
-		Runtime.getRuntime().exec(command, environment);
-
-		// Let application run for eternity. It will be eventually
-		// stopped when systemd service stops.
-		Thread.sleep(1_000_000_000);
-
-		System.out.println("Application exit.");
+		processBuilder.environment().putIfAbsent("XDG_RUNTIME_DIR", "/run/user/" + new UnixSystem().getUid());
+		// Redirect process I/O to the application's I/O so it's output can be
+		// observed by journalctl.
+		processBuilder.inheritIO();
+		processBuilder.start();
 	}
 }
 
